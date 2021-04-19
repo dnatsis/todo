49f6751c5b907762cbfc3f77f7a2df7481ec7283
[Dhimitris Natsis] <[dnatsis@worcester.edu]>
2021-04-19 16:35:18 -0400

    Setup for Heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import todosRoutes from './routes/todosRoutes.js';
@@ -10,13 +11,23 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/api/todos', todosRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.listen(
   PORT,
